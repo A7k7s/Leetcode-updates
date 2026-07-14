1684b7172932781907520744c79fb73c6fae6826
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2026-07-14 14:03:03 +0530

    Create 350-IntersectionOfTwoArraysIi

---
+++ 350-IntersectionOfTwoArraysIi/350-IntersectionOfTwoArraysIi.py
@@ -0,0 +1,18 @@
+# Last updated: 7/14/2026, 2:02:56 PM
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        res=[]
+        i=0
+        j=0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                i+=1
+            elif nums2[j]<nums1[i]:
+                j+=1
+            else:
+                res.append(nums1[i])
+                i+=1
+                j+=1
+        return res
