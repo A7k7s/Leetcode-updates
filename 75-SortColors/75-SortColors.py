85404d6ae7877853146b29b868335fa32e4aec00
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2026-07-14 14:05:17 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.py
@@ -0,0 +1,10 @@
+# Last updated: 7/14/2026, 2:05:10 PM
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(0, len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
