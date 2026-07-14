e2863a84448e16e997b6ecd16b0333bd6aa08a6e
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2026-07-14 14:04:56 +0530

    Create 112-PathSum

---
+++ 112-PathSum/112-PathSum.py
@@ -0,0 +1,10 @@
+# Last updated: 7/14/2026, 2:04:49 PM
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return targetSum == root.val
+        left_sum = self.hasPathSum(root.left, targetSum - root.val)
+        right_sum = self.hasPathSum(root.right, targetSum - root.val)
+        return left_sum or right_sum
