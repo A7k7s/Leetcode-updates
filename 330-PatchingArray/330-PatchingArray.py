228a8118e7aac7c659a3a1654318f0460cf0fc6d
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2026-07-14 14:03:17 +0530

    Create 330-PatchingArray

---
+++ 330-PatchingArray/330-PatchingArray.py
@@ -0,0 +1,14 @@
+# Last updated: 7/14/2026, 2:03:10 PM
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        miss = 1
+        result = 0
+        i = 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                result += 1
+        return result
