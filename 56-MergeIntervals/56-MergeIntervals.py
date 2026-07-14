e994c5a0d2a325f7548cb57db78cef01b7aefd3f
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2026-07-14 14:05:35 +0530

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -0,0 +1,14 @@
+# Last updated: 7/14/2026, 2:05:28 PM
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:        
+        merged = []
+        intervals.sort(key=lambda x: x[0])
+        prev = intervals[0]
+        for interval in intervals[1:]:
+            if interval[0] <= prev[1]:
+                prev[1] = max(prev[1], interval[1])
+            else:
+                merged.append(prev)
+                prev = interval
+        merged.append(prev)
+        return merged
