e923a84a276b379e97813a01c9eefba416890ac1
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2026-07-14 14:05:25 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.py
@@ -0,0 +1,10 @@
+# Last updated: 7/14/2026, 2:05:18 PM
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        v=sqrt(x)
+        return int(v)
+        
