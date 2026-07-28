4fead40e9f8af5394ae00673d874b0f990121b1b
Akshayaa Panneerselvam <145751636+A7k7s@users.noreply.github.com>
2026-07-28 11:14:35 +0530

    File updated at 7/28/2026, 11:14:25 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,17 +1,14 @@
-# Last updated: 7/28/2026, 11:14:07 AM
+# Last updated: 7/28/2026, 11:14:25 AM
 1class Solution:
 2    def productExceptSelf(self, nums):
 3        n = len(nums)
 4        output = [1] * n
-5
-6        left = 1
-7        for i in range(n):
-8            output[i] *= left
-9            left *= nums[i]
-10
-11        right = 1
-12        for i in range(n - 1, -1, -1):
-13            output[i] *= right
-14            right *= nums[i]
-15
-16        return output
+5        left = 1
+6        for i in range(n):
+7            output[i] *= left
+8            left *= nums[i]
+9        right = 1
+10        for i in range(n - 1, -1, -1):
+11            output[i] *= right
+12            right *= nums[i]
+13        return output
